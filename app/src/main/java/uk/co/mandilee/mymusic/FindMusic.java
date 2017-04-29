ff7c48c8f3e03ff6d802f0fb36dfbb996ce92925
Mandi Lee <github@mandilee.co.uk>
2017-04-29 19:54:48 +0100

    adding buttons

--- app/src/main/java/uk/co/mandilee/mymusic/FindMusic.java
+++ app/src/main/java/uk/co/mandilee/mymusic/FindMusic.java
@@ -1,13 +1,34 @@
 package uk.co.mandilee.mymusic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class FindMusic extends AppCompatActivity {
+public class FindMusic extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_music);
+
+        Button btnNowPlaying = (Button) findViewById(R.id.now_playing);
+
+        btnNowPlaying.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent nextActivity = null;
+        switch (v.getId()) {
+            case R.id.now_playing:
+                nextActivity = new Intent(this, NowPlaying.class);
+                break;
+        }
+
+        if (nextActivity != null) {
+            startActivity(nextActivity);
+        }
     }
 }
--- app/src/main/java/uk/co/mandilee/mymusic/NowPlaying.java
+++ app/src/main/java/uk/co/mandilee/mymusic/NowPlaying.java
@@ -1,13 +1,40 @@
 package uk.co.mandilee.mymusic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class NowPlaying extends AppCompatActivity {
+public class NowPlaying extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
+
+        Button btnArtistInfo = (Button) findViewById(R.id.artist_info);
+        Button btnFindMusic = (Button) findViewById(R.id.find_music);
+
+        btnArtistInfo.setOnClickListener(this);
+        btnFindMusic.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent nextActivity = null;
+        switch (v.getId()) {
+            case R.id.artist_info:
+                nextActivity = new Intent(this, ArtistInfo.class);
+                break;
+
+            case R.id.find_music:
+                nextActivity = new Intent(this, FindMusic.class);
+                break;
+        }
+
+        if (nextActivity != null) {
+            startActivity(nextActivity);
+        }
     }
 }
